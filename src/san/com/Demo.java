df37751a7e8d1127c0231522813c9e703b9a8f9a
Santosh2411 <51736372+Santosh2411@users.noreply.github.com>
2022-12-16 10:18:56 +0530

    Update Demo.java
    m2 method added

--- src/san/com/Demo.java
+++ src/san/com/Demo.java
@@ -7,6 +7,11 @@ public class Demo {
 		
 	  System.out.println("this is  m1 method");
 	}
+	public void m2()
+	{
+		
+	  System.out.println("this is  m2 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("this is for pull");
 
